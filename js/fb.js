2b5e20a7727078665af3cec34c19046dba28c11f
Mefteg <corexpertmacmini@Mac-mini-de-CorexpertMacMini.local>
2013-10-11 14:03:48 +0200

    Ajout du generateur d'ennemis

--- js/fb.js
+++ js/fb.js
@@ -18,7 +18,6 @@ window.fbAsyncInit = function() {
 			// have logged in to the app.
 			testAPI();
 			console.log("CONNECTED");
-			init();
 		} else if (response.status === 'not_authorized') {
 			// In this case, the person is logged into Facebook, but not into the app, so we call
 			// FB.login() to prompt them to do so. 
--- js/main.js
+++ js/main.js
@@ -27,6 +27,59 @@ function init() {
 		}).
 	color("rgb(255, 0, 255)").
 		attr({w: SIZE, h: SIZE});
+	entity.bind("EnterFrame", function() {
+		if (this.x < 0) {
+			this.x = 0;
+		}
+		if (this.x + SIZE > WIDTH) {
+			this.x = WIDTH - SIZE;
+		}
+		if (this.y < 0) {
+			this.y = 0;
+		}
+		if (this.y + SIZE > HEIGHT) {
+			this.y = HEIGHT - SIZE;
+		}
+	});
+
+	var summon = Crafty.c("Summon", {
+		mCpt: 0,
+
+	    init: function() {
+		    this.bind("EnterFrame", function() {
+			this.mCpt += 1;
+			if (this.mCpt >= 100) {
+				console.log("NEW ENEMY");
+				this.createEnemy();
+				this.mCpt = 0;
+			}
+		    });
+	    },
+
+	    summon: function(_nbEnemiesAtStart) {
+	    },
+
+	    createEnemy: function() {
+		    var e = Crafty.e("2D, Canvas, RandomPosition, Color, Collision").color("rgb(255, 0, 0)").attr({w: SIZE, h: SIZE, dX: -5});
+		    e.bind("EnterFrame", function() {
+			    if (GAME_OVER == false) {
+				    this.x += this.dX;
+
+				    if (this.x < -SIZE) {
+					    this.x = Crafty.math.randomInt(WIDTH + SIZE, WIDTH * 1.5);
+					    this.y = Crafty.math.randomInt(0, HEIGHT - SIZE);
+				    }
+			    }
+		    });
+		    e.onHit("Paddle", function() {
+			    if (GAME_OVER == false) {
+				    console.log("GAME OVER");
+				    GAME_OVER = true;
+			    }
+		    });
+	    }
+	});
+	var manager = Crafty.e("Summon").summon(10);
 
 	for (var i=0; i<5; i++) {
 		var e = Crafty.e("2D, Canvas, RandomPosition, Color, Collision").color("rgb(255, 0, 0)").attr({w: SIZE, h: SIZE, dX: -5});
@@ -65,3 +118,4 @@ function init() {
 		});
 }
 
+init();
