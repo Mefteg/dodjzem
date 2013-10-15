8ceb5e01e57efbf43975c4b4193d9e8924003e92
Tom <tom@roucoups>
2013-10-15 08:16:06 +0200

    Ajout du score dans l'écran de fin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,56 @@
+var WIDTH = 690;
+var HEIGHT = 360;
+var GAME_OVER = false;
+var SCORE = 0;
+var SIZE = 20;
+
+Crafty.c("Summon", {
+	mCpt: 0,
+
+	init: function() {
+		this.bind("EnterFrame", function() {
+			if (GAME_OVER == false) {
+				this.mCpt += 1;
+				if (this.mCpt >= 100) {
+					console.log("NEW ENEMY");
+					this.createEnemy();
+					this.mCpt = 0;
+				}
+			}
+		});
+	},
+
+	summon: function(_nbEnemiesAtStart) {
+		for (var i=0; i<_nbEnemiesAtStart; i++) {
+			this.createEnemy();
+		}
+	},
+
+	createEnemy: function() {
+		var e = Crafty.e("2D, Canvas, RandomPosition, Color, Collision").color("rgb(255, 255, 255)").attr({w: SIZE, h: SIZE, dX: -5});
+		e.bind("EnterFrame", function() {
+			if (GAME_OVER == false) {
+				this.x += this.dX;
+
+				if (this.x < -SIZE) {
+					this.x = Crafty.math.randomInt(WIDTH + SIZE, WIDTH * 1.5);
+					this.y = Crafty.math.randomInt(0, HEIGHT - SIZE);
+				}
+			}
+		});
+		e.onHit("Paddle", function() {
+			if (GAME_OVER == false) {
+				console.log("GAME OVER");
+				GAME_OVER = true;
+				SCORE = Crafty("Score").score;
+				Crafty.scene("end");
+			}
+		});
+	}
+});
+
 function init() {
-	var WIDTH = 690;
-	var HEIGHT = 360;
-	var GAME_OVER = false;
-	var SIZE = 20;
+
 
 	Crafty.init(WIDTH, HEIGHT);
 	Crafty.background('rgb(127,127,127)');
@@ -61,8 +109,11 @@ function init() {
 			}
 		});
 
+		var button = Crafty.e("Button, Text").attr({x: 100, y: 100, w: 200}).
+		text("Score: " + SCORE).
+		css({"text-align": "center"});
 
-		var button = Crafty.e("Button, Text").attr({x: 100, y: 100}).
+		var button = Crafty.e("Button, Text").attr({x: 100, y: 140}).
 		text("Restart").
 		css({"text-align": "center"});
 		button.onClick = function(_mouseEvent) {
@@ -106,49 +157,6 @@ function init() {
 		}
 	});
 
-	var summon = Crafty.c("Summon", {
-		mCpt: 0,
-
-	    init: function() {
-		    this.bind("EnterFrame", function() {
-			    if (GAME_OVER == false) {
-				    this.mCpt += 1;
-				    if (this.mCpt >= 100) {
-					    console.log("NEW ENEMY");
-					    this.createEnemy();
-					    this.mCpt = 0;
-				    }
-			    }
-		    });
-	    },
-
-	    summon: function(_nbEnemiesAtStart) {
-		    for (var i=0; i<_nbEnemiesAtStart; i++) {
-			    this.createEnemy();
-		    }
-	    },
-
-	    createEnemy: function() {
-		    var e = Crafty.e("2D, Canvas, RandomPosition, Color, Collision").color("rgb(255, 255, 255)").attr({w: SIZE, h: SIZE, dX: -5});
-		    e.bind("EnterFrame", function() {
-			    if (GAME_OVER == false) {
-				    this.x += this.dX;
-
-				    if (this.x < -SIZE) {
-					    this.x = Crafty.math.randomInt(WIDTH + SIZE, WIDTH * 1.5);
-					    this.y = Crafty.math.randomInt(0, HEIGHT - SIZE);
-				    }
-			    }
-		    });
-		    e.onHit("Paddle", function() {
-			    if (GAME_OVER == false) {
-				    console.log("GAME OVER");
-				    GAME_OVER = true;
-				    Crafty.scene("end");
-			    }
-		    });
-	    }
-	});
 	var manager = Crafty.e("2D, Summon").summon(10);
 
 	var score = Crafty.e("Score, 2D, DOM, Text").attr({x: 590, y: 0, w: 100, h: 50, score: 0});
